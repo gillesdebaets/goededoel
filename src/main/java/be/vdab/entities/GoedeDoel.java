c94adff464d794fa73828c193e64a481290f2f39
Gilles <gillesdebaets@gmail.com>
2015-02-23 09:54:35 +0100

    javadoc toevoegen

--- src/main/java/be/vdab/entities/GoedeDoel.java
+++ src/main/java/be/vdab/entities/GoedeDoel.java
@@ -2,18 +2,39 @@ package be.vdab.entities;
 
 import java.math.BigDecimal;
 
+
+/**
+ * Een <strong>goed doel</strong> waarvoor men geld inzamelt
+ * @author Gilles
+ */
 public class GoedeDoel {
 	private final String naam;
 	private BigDecimal opgebracht = BigDecimal.ZERO;
+	
+	/**
+	 * Maakt een GoedeDoel object
+	 * @param naam De naam van het goede doel
+	 */
 	public GoedeDoel(String naam) {
 		this.naam = naam;
 	}
+	
+	/**
+	 * Geeft de naam terug
+	 * @return de naam
+	 */
 	public String getNaam() {
 		return naam;
 	}
+	
+	/**
+	 * Geeft terug hoeveel &euro; dit doel heeft opgebracht
+	 * @return het opgebracht bedrag
+	 */
 	public BigDecimal getOpgebracht() {
 		return opgebracht;
 	}
+	
 	@Override
 	public boolean equals(Object object) {
 	if (!(object instanceof GoedeDoel)) {
@@ -21,6 +42,7 @@ public class GoedeDoel {
 	}
 	return ((GoedeDoel) object).naam.equalsIgnoreCase(naam);
 	}
+	
 	@Override
 	public int hashCode() {
 		return naam.toUpperCase().hashCode();
